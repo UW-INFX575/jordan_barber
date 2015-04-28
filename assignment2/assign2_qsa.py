a7335317b72a5365b15ae2b392a6d5f2b163d7fc
Jordan Barber <you@example.com>
2015-04-28 07:43:22 -0700

    qsa script

---
+++ assignment2/assign2_qsa.py
@@ -0,0 +1,15 @@
+import boto
+from boto.s3.connection import S3Connection
+import boto.s3.connection
+
+access_key = ''
+secret_key = ''
+
+bucket_name = access_key.lower() + '-dump'
+conn = boto.connect_s3(access_key, secret_key)
+
+mybucket = conn.get_bucket('jordan-barber')
+
+plans_key = mybucket.get_key('total_trigrams.csv')
+plans_url = plans_key.generate_url(3600, query_auth=True, force_http=True)
+print(plans_url)
